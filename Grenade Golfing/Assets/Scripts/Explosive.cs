6b8801616672ff51d0341cd2280f614547d513f3
Alexander Amling <aja7073@rit.edu>
2018-10-28 22:02:33 -0400

    Proof of Concept
    Basic level model
    Grenade model
    Gold ball physics
    Basic explosives behavior
    -used untiy addExplosiveForce with adjustment for vertical force values to minimize the vertical movement of the ball

---
+++ Grenade Golfing/Assets/Scripts/Explosive.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosive : MonoBehaviour {
+
+    public GameObject Player;
+    public float Delay;
+    public float Power;
+    public float Radius;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Delay -= Time.deltaTime;
+        if(Delay <= 0)
+        {
+            Explode();
+        }
+	}
+
+    void Explode()
+    {
+        Player.GetComponent<Rigidbody>().AddExplosionForce(Power, transform.position, Radius, -.5f);
+        Destroy(gameObject);
+    }
+}
