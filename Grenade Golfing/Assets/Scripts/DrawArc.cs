12fd4a77e43af3d9e99497189204eab866e95adc
Alexander Amling <aja7073@rit.edu>
2018-11-01 22:19:52 -0400

    Parabola Drawing Script
    Created a script that draws a parabola between two points along the x-axis with a given height. The next step is to make it work in both x and z axis for diagonal arcs

---
+++ Grenade Golfing/Assets/Scripts/DrawArc.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class DrawArc : MonoBehaviour {
+    LineRenderer lr;
+
+    public float arcHeight;
+    public int resolution;
+
+    // aiming vectors
+    public Vector3 target;
+
+    void Awake () {
+        lr = GetComponent<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RenderArc();
+	}
+
+    void RenderArc() {
+        lr.positionCount = resolution + 1;
+        lr.SetPositions(CalculateArcArr());
+    }
+
+    Vector3[] CalculateArcArr() {
+        Vector3[] arcArr = new Vector3[resolution + 1];
+        float xRange = target.x - transform.position.x;
+        float zRange = target.z - transform.position.z;
+        for (int i = 0; i < arcArr.Length; i++)
+        {
+            float x = transform.position.x + (xRange / (float)resolution) * i;
+            // y = ((-x^2 + xRange * x) * 4 * arcHeight) / xRange^2
+            float y = ((-x * x + xRange * x) * 4 * arcHeight) / (xRange * xRange);
+            float z =  transform.position.z + (zRange / (float)resolution) * i;
+            arcArr[i] = new Vector3(x, y, z);
+        }
+        return arcArr;
+    }
+}
