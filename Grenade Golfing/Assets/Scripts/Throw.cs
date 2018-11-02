fb0fb763b132bae0a41872b6095ac14ce267735f
Alexander Amling <aja7073@rit.edu>
2018-11-01 22:35:53 -0400

    Throwing Along Drawn Arc
    Pressing 't' in the testing environment will throw a grenade along the drawn curve, even after moving or adjusting the target or height.
    Drawing works in both x and z axis, but is currently fixed to the xz-plane where both ends are a y=0.

---
+++ Grenade Golfing/Assets/Scripts/Throw.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throw : MonoBehaviour {
+
+    public ArcTest throwable;
+    public float throwForce;
+
+    LineRenderer lr;
+
+	// Use this for initialization
+	void Start () {
+        lr = GetComponent<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            ArcTest thrownObject = Instantiate(throwable, lr.GetPosition(0), Quaternion.identity);
+            thrownObject.forceScale = throwForce;
+            thrownObject.launchForce = lr.GetPosition(1) - lr.GetPosition(0);
+        }
+	}
+}
