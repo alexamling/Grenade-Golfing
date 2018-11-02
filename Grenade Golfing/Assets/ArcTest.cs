2a9369e420019355a658e3c65ec274ee3f0fb549
Alexander Amling <aja7073@rit.edu>
2018-11-01 20:44:04 -0400

    Testing for Arc Aiming
    Created a scene for the purpose of testing object trajectory and ways to model this trajectory for aiming purposes. The method I've found seems rather primative, but I think it is functional enough for the sake of this project.

---
+++ Grenade Golfing/Assets/ArcTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArcTest : MonoBehaviour {
+
+    public float forceScale;
+    public Vector3 launchForce;
+
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<Rigidbody>().velocity = launchForce * forceScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
